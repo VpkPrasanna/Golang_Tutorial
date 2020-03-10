a5681a6ea207ee357cdfcf64def76fab47fdf691
VpkPrasanna <prasannakumarv@teknuance.com>
2020-03-10 22:44:11 +0530

    initial Comit

---
+++ basics/var.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var batman string = "I am batman"
+	fmt.Println(batman)
+
+	var superman string
+	superman = "I am superman"
+	fmt.Println(superman)
+
+	thor := "I am thor"
+	fmt.Println(thor)
+
+	// thorRating := 3.
+	// fmt.Printf("%v, %T", thorRating, thorRating)
+
+	var Ironman, CatAmerica string = "I am Ironman", "I am capt America"
+	fmt.Println(Ironman, CatAmerica)
+
+	var defaultValue string
+	fmt.Println(defaultValue)
+
+	var (
+		spiderman = "I am spiderman"
+		age       = 18
+		powers    = "web slings, spidy sense"
+		antman    = "I am antman"
+	)
+	fmt.Println(spiderman, age, powers, antman)
+
+}
