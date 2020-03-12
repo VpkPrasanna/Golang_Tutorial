9690b040b1c9fe90cd1aba3f5adf508d602d2a38
VpkPrasanna <prasannakumarv@teknuance.com>
2020-03-12 23:45:23 +0530

    user input added

--- basics/var.go
+++ basics/var.go
@@ -1,33 +1,31 @@
 package main
 
-import "fmt"
+// func main() {
+// 	var batman string = "I am batman"
+// 	fmt.Println(batman)
 
-func main() {
-	var batman string = "I am batman"
-	fmt.Println(batman)
+// 	var superman string
+// 	superman = "I am superman"
+// 	fmt.Println(superman)
 
-	var superman string
-	superman = "I am superman"
-	fmt.Println(superman)
+// 	thor := "I am thor"
+// 	fmt.Println(thor)
 
-	thor := "I am thor"
-	fmt.Println(thor)
+// 	// thorRating := 3.
+// 	// fmt.Printf("%v, %T", thorRating, thorRating)
 
-	// thorRating := 3.
-	// fmt.Printf("%v, %T", thorRating, thorRating)
+// 	var Ironman, CatAmerica string = "I am Ironman", "I am capt America"
+// 	fmt.Println(Ironman, CatAmerica)
 
-	var Ironman, CatAmerica string = "I am Ironman", "I am capt America"
-	fmt.Println(Ironman, CatAmerica)
+// 	var defaultValue string
+// 	fmt.Println(defaultValue)
 
-	var defaultValue string
-	fmt.Println(defaultValue)
+// 	var (
+// 		spiderman = "I am spiderman"
+// 		age       = 18
+// 		powers    = "web slings, spidy sense"
+// 		antman    = "I am antman"
+// 	)
+// 	fmt.Println(spiderman, age, powers, antman)
 
-	var (
-		spiderman = "I am spiderman"
-		age       = 18
-		powers    = "web slings, spidy sense"
-		antman    = "I am antman"
-	)
-	fmt.Println(spiderman, age, powers, antman)
-
-}
+// }
