3865198f7b420042618617c9127307710e34a2d9
VpkPrasanna <prasannakumarv@teknuance.com>
2020-03-13 13:01:00 +0530

    added comments to the take input file

--- basics/02takeInput.go
+++ basics/02takeInput.go
@@ -10,19 +10,23 @@ import (
 
 func main() {
 
+	// this set of lines take input of one word or one number if there is more than one word or number ot will throw error
 	// var myString string
 	// fmt.Scanln(&myString)
 	// fmt.Println(myString)
 
+	// if you want to store the printed value in any varible it will throw an error to treat this error we are using _ varible seperately
 	// var name string = "hitesh"
 	// var a, _ = fmt.Println(name)
 	// fmt.Println(a)
 
+	// := is called as short variable declarations
 	// reader := bufio.NewReader(os.Stdin)
 	// fmt.Print("Enter you full name: ")
 	// myname, _ := reader.ReadString('\n')
 	// fmt.Println(myname)
 
+	// incase you need add 2 to the input rating you need to do like this so that golang will accept this pattern
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter your rating: ")
 	myrating, _ := reader.ReadString('\n')
