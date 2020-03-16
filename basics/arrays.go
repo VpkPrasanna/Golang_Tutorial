4c964750b696b65bee7b9ace687ed06bba94cdd8
VpkPrasanna <prasannakumarv@teknuance.com>
2020-03-16 23:39:23 +0530

    added code for slices in golang

--- basics/arrays.go
+++ basics/arrays.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
+// func main() {
+// 	var numbers [3]string
+// 	numbers[0] = "prasanna"
+// 	numbers[1] = "loves"
+// 	numbers[2] = "arumuga"
 
-func main() {
-	var numbers [3]string
-	numbers[0] = "prasanna"
-	numbers[1] = "loves"
-	numbers[2] = "arumuga"
+// 	fmt.Println(numbers)
 
-	fmt.Println(numbers)
-
-	var colors = [3]string{"prasanna", "loves", "arumugapriya"}
-	fmt.Println(colors)
-}
+// 	var colors = [3]string{"prasanna", "loves", "arumugapriya"}
+// 	fmt.Println(colors)
+// }
