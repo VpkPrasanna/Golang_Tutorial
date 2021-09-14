bb55e561bbe5b30f593571eef9b891ce87a7befa
VpkPrasanna <v.prasannakumar1998@gmail.com>
2021-09-14 23:25:07 +0530

    Lesson on time Module

---
+++ mygolang-LCO/06mytime/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to Time Package ")
+
+	presentTime := time.Now()
+	// This will give us the result in ugly format like -> The time is  2021-09-14 23:11:08.2579918 +0530 IST m=+0.004796901
+	fmt.Println("The time is ", presentTime)
+	// We will format this , to see in a better way like . : We need to follow this syntax to format the date
+	updatedTIme := presentTime.Format("01-02-2006 15:04:05 Monday ")
+	fmt.Println("The prettier version ,", updatedTIme)
+
+	//Lets create a date
+	newDate := time.Date(2020, time.July, 1, 23, 23, 0, 0, time.UTC)
+	fmt.Println("The created date is ,", newDate)
+	fmt.Println("The formatted date is ,", newDate.Format("01-02-2006 Monday"))
+}
