2214219fcade436a95968305a3e65bc97ff6d795
VpkPrasanna <v.prasannakumar1998@gmail.com>
2021-09-23 23:01:19 +0530

    An intro to Arrays

---
+++ mygolang-LCO/08myarrays/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Arrays")
+
+	var fruitList [4]string
+	fruitList[0] = "Apple"
+	fruitList[1] = "Tomato"
+	fruitList[3] = "Peach"
+	// The result will be like this The values in the array is ,: [Apple Tomato  Peach],
+	// where the space between tomato and peach is 2 since it has alloted memory for 3 rd postition
+	fmt.Println("The values in the array is ,:", fruitList)
+
+	// This will give us the answer as 4 as we have initialized with 4 as the total input and it will create a an array of 4 inp with memory
+	fmt.Println("The len of the fruit list is:,", len(fruitList))
+
+	// Another way to define the sttings
+
+	var vegList = [5]string{"Potato", "tomato", "beans"}
+	fmt.Println("The Veglist is :,", vegList)
+	fmt.Println("The len of the veg list is :", len(vegList))
+
+}
