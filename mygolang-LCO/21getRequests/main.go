3275dac0ddb713aa11cb2ebee75f91120d15fcd6
VpkPrasanna <v.prasannakumar1998@gmail.com>
2021-10-17 23:21:24 +0530

    POST Methond in golng

--- mygolang-LCO/21getRequests/main.go
+++ mygolang-LCO/21getRequests/main.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 	fmt.Println("Welcome to Get request in golang ")
-	performGetRequest()
+	// Function call for GET request
+	// performGetRequest()
+
+	// Function call for POST request
+	perfromJsonPostRequest()
 }
 
 func performGetRequest() {
@@ -31,4 +35,23 @@ func performGetRequest() {
 
 	// Regular way to print the content as output
 	// fmt.Println("The content in the url is :", string(content))
+}
+
+func perfromJsonPostRequest() {
+	const myurl string = "http://localhost:8000/post"
+
+	//create fake json
+	fakedata := strings.NewReader(`
+	{
+		"Name":"VPK"
+	}`)
+
+	res, err := http.Post(myurl, "application/json", fakedata)
+
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	contentByte, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(contentByte))
 }
