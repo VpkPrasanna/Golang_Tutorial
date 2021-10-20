3b3130fb0ad1677a4f974d8a0ac8887ed229dda5
VpkPrasanna <v.prasannakumar1998@gmail.com>
2021-10-20 10:53:02 +0530

    decoding json in golang

--- mygolang-LCO/22MoreonJson/main.go
+++ mygolang-LCO/22MoreonJson/main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	fmt.Println("Welcome to Explore more on JSON in golang")
-	EncodeJson()
+	// EncodeJson()
+
+	DecodeJson()
 }
 
 // `` These are aliasis of the keys present in the struct, we can create in any name as we need
@@ -26,9 +28,42 @@ func EncodeJson() {
 		{"Angular", 299, "lco.dev", "vpk123", nil},
 	}
 	// Packing the data into JSON
+	// MarshallIndent will Indent the json data with the specified data format
 	response, err := json.MarshalIndent(lcoCourses, "", "\t")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("%s \n", response)
+}
+
+// Decode the json
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJs",
+		"Price": 299,
+		"website": "lco.dev",
+		"tags": ["web-dev","js"]
+	}
+	
+	`)
+	var lcoCourse course
+	validJson := json.Valid(jsonDataFromWeb)
+	if validJson {
+		fmt.Println("The json is valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v \n", lcoCourse)
+	} else {
+		fmt.Println("IT IS NOT A VALID JSON")
+	}
+
+	// some cases where you just want to add data to key value
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v \n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("The key is %v and the value is %v and the Type of value is %T \n", k, v, v)
+	}
+
 }
